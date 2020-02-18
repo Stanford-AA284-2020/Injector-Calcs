ed1a2a55f39a1c1ca4584186bbc68dfa00c30cdb
Jeff Robinson <47460450+Jeff-Robinson@users.noreply.github.com>
2020-02-18 01:27:41 -0800

    using gas system sim output

--- InjectorSizing.m
+++ InjectorSizing.m
@@ -1,11 +1,11 @@
 %% Main Injector Sizing
-addpath('/Users/JBR132/Documents/_Stanford/AA284B Propulsion System Design Lab/MATLAB-PnID/')
+addpath('D:\Documents\GitHub\MATLAB-PnID\Gas Feed System Model\')
 FluidDatabase
 
-IdP = 0.2;
+IdP = 0.32;
 
-PIF = 10/(1-IdP)*1e5;% Pa, Methane injector feed pressure
-TIF = 230;% K, Methane injector feed temperature
+PIF = 1.6205e6;% Pa, Methane injector feed pressure
+TIF = 177.42;% K, Methane injector feed temperature
 rhoIF = PREoS(Methane,"rho",PIF,TIF);% In manifold
 rhoIF2 = PREoS(Methane,"rho",PIF*(1-IdP),TIF);% After orifice
 
@@ -16,19 +16,19 @@ rhoIO = rhoIO(2);
 rhoIO2 = PREoS(Oxygen,"rho",PIO*(1-IdP),TIO);% After orifice
 rhoIO2 = rhoIO2(2);
 
-n_elements = 6;
-mdotF = 0.108140110052637;% kg/s, Methane mass flow rate
-mdotO = 0.270350275131593;% kg/s, Oxygen mass flow rate
+n_elements = 10;
+mdotF = 0.07945;% kg/s, Methane mass flow rate
+mdotO = 0.1986;% kg/s, Oxygen mass flow rate
 
 CdIF = 0.6;% Cd of thin plate orifice
 CdIO = 0.6;
 
-AIF = mdotF/(CdIF*sqrt(2*rhoIF*(PIF-0.8*PIF)));% From Cd equation
-AIO = mdotF/(CdIO*sqrt(2*rhoIO*(PIO-0.8*PIO)));
+AIF = mdotF/(CdIF*sqrt(2*rhoIF*(PIF*IdP)));% From Cd equation
+AIO = mdotF/(CdIO*sqrt(2*rhoIO*(PIO*IdP)));
 
 % Injector orifice diameters
 DIO = sqrt((AIO/n_elements)/pi)*2;% n circular orifices
-t = max([PIO*(1-IdP)*DIO/2*1/convpres(125e3,"psi","Pa"),0.4e-3]);
+t = max([PIO*(1-IdP)*DIO/2*1/convpres(125e3,"psi","Pa"),0.381e-3]);
 DIFi = DIO+2*t;
 % AIF = pi*(DIFo/2)^2 - pi*(DIFi/2)^2
 DIFo = sqrt((AIF/n_elements + pi*(DIFi/2)^2)/pi)*2;
@@ -38,14 +38,14 @@ VIF = mdotF/(rhoIF2*AIF);
 VIO = mdotO/(rhoIO2*AIO);
 
 % Momentum ratio
-dp = VIF*mdotF/(VIO*mdotO);
+dp = VIO*mdotO/(VIF*mdotF);
 
 fprintf(['Ox Orifice Diameter: %0.2f mm \n',...
     'Fuel Annulus Inner Diameter: %0.2f mm \n',...
     'Fuel Annulus Outer Diameter: %0.2f mm \n',...
     'Ox Injection Velocity: %0.2f m/s \n',...
     'Fuel Injection Velocity: %0.2f m/s \n',...
-    'F/O Momentum Ratio: %0.4f\n'],...
+    'O/F Momentum Flux Ratio: %0.4f\n'],...
     DIO*1000,DIFi*1000,DIFo*1000,VIO,VIF,dp)
 
 ts = deg2rad(0:1:360);
